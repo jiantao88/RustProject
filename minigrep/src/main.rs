b62eaa12397f55e69b4b1ab77198def46de17139
jiantao <a_tao123@163.com>
2022-03-04 07:39:12 +0800

    eprint

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,20 +1,19 @@
+use minigrep::Config;
 use std::env;
 use std::process;
-use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     // let config = parse_config(&args);
     let config = Config::new(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {}", err);
+        eprintln!("Problem parsing arguments: {}", err);
         process::exit(1);
     });
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.filename);
-    if let Err(e)= minigrep::run(config){
-        println!("Application error: {}", e);
+    if let Err(e) = minigrep::run(config) {
+        eprintln!("Application error: {}", e);
         process::exit(1);
     };
 }
-
