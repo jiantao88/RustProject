5e0680a22257f5a6045608201f7915de1585fe5f
jiantao <a_tao123@163.com>
2022-05-26 16:12:06 +0800

    替换使用迭代器方式

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,19 +1,26 @@
+use std::env;
 use std::error::Error;
 use std::fs;
-use std::env;
 pub struct Config {
     pub query: String,
     pub filename: String,
     pub case_sensitive: bool,
 }
 impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enougth args");
-        }
-        let query = args[1].to_string();
-        let filename = args[2].to_string();
+    pub fn new(mut args: env::Args) -> Result<Config, &'static str> {
+        args.next();
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let filename = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file name"),
+        };
+
         let case_sensitive = env::var("CASE_INSENSITIVE").is_err();
+
         Ok(Config {
             query,
             filename,
