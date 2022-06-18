0715b68654b532889b64ec7d8fc0b184a598a9f6
jiantao <a_tao123@163.com>
2022-06-18 08:58:14 +0800

    webserver

--- webServer/src/main.rs
+++ webServer/src/main.rs
@@ -1,12 +1,19 @@
-use std::{net::{TcpListener, TcpStream}, io::{Read, Write}, fs};
+use std::{net::{TcpListener, TcpStream}, io::{Read, Write}, fs, time::Duration, thread};
+
+use webServer::ThreadPool;
+
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
+    let pool = ThreadPool::new(4);
+
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        handler_connection(stream);
+        pool.execute(||{
+            handler_connection(stream);
+        })
     
     }
 
@@ -15,34 +22,48 @@ fn main() {
         stream.read(&mut buffer).unwrap();
 
         let get = b"GET / HTTP/1.1\r\n";
+        let sleep = b"GET /sleep HTTP/1.1\r\n";
+        let (status_line,filename) = if buffer.starts_with(get) {
+            ("HTTP/1.1 200 OK","hello.html") 
+        }else if buffer.starts_with(sleep) {
+            thread::sleep(Duration::from_secs(5));
+            ("HTTP/1.1 200 OK","hello.html") 
+        }else{
+            ("HTTP/1.1 404 NOT FOUND","404.html")
+        };
+        let contents = fs::read_to_string(filename).unwrap();
 
-        if buffer.starts_with(get){
-            let contents= fs::read_to_string("hello.html").unwrap();
-            let response = format!(
-                "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
-                contents.len(),
-                contents
-            );
-            stream.write(response.as_bytes()).unwrap();
-            stream.flush().unwrap();
-    
-        } else {
-            let status_line = "HTTP/1.1 404 NOT FOUND";
-            let contents = fs::read_to_string("404.html").unwrap();
-    
-            let response = format!(
-                "{}\r\nContent-Length: {}\r\n\r\n{}",
-                status_line,
-                contents.len(),
-                contents
-            );
-    
-            stream.write(response.as_bytes()).unwrap();
-            stream.flush().unwrap();
-        }   
+        let response = format!(
+            "{}\r\nContent-Length: {}\r\n\r\n{}",
+            status_line,
+            contents.len()
+            ,contents
+        );
 
-       
+        stream.write(response.as_bytes()).unwrap();
+        stream.flush().unwrap();
 
+   
+        // if buffer.starts_with(get){
+        //     let contents= fs::read_to_string("hello.html").unwrap();
+        //     let response = format!(
+        //         "HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+        //         contents.len(),
+        //         contents
+        //     );
+        //     stream.write(response.as_bytes()).unwrap();
+        //     stream.flush().unwrap();
+    
+        // } else {
+        //     let status_line = "HTTP/1.1 404 NOT FOUND";
+        //     let contents = fs::read_to_string("404.html").unwrap();
+    
+        //     let response = format!(
+        //         "{}\r\nContent-Length: {}\r\n\r\n{}",
+        //         status_line,
+        //         contents.len(),
+        //         contents
+        //     );
         // println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
     }
